2a3e3750b361e7d7591a7e3f61bc4d1c00838945
Giorgos Ath <geotha1995@hotmail.com>
2015-07-10 16:10:14 +0300

    etoimo to gameplay

--- Find4/Find4Page.xaml.cs
+++ Find4/Find4Page.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -41,7 +42,7 @@ namespace Find4
             round = 0;
             cur_pos = 0;
             color = new colors();
-            option = new Block(/*op1,op2,op3,op4*/);
+            option = new Block(op1,op2,op3,op4);
             panel = new ComboBlock[6];
             panel[0] = new ComboBlock(ans1_1, ans1_2, ans1_3, ans1_4, que1_1, que1_2, que1_3, que1_4);
             panel[1] = new ComboBlock(ans2_1, ans2_2, ans2_3, ans2_4, que2_1, que2_2, que2_3, que2_4);
@@ -58,7 +59,7 @@ namespace Find4
             {
                 if (ellip != null)
                 {
-                    panel[round].que[3 - cur_pos].Fill = ellip.Fill;
+                    panel[round].que[cur_pos].Fill = ellip.Fill;
 
                 }
                 cur_pos++;
@@ -67,11 +68,12 @@ namespace Find4
             {
                 if (ellip != null)
                 {
-                    panel[round].que[3 - cur_pos].Fill = ellip.Fill;
+                    panel[round].que[cur_pos].Fill = ellip.Fill;
 
                 }
-                if (check())
-                    win();
+                if (check()){
+                    //win();
+                }
                 cur_pos = 0;
                 round++;
             }
@@ -84,39 +86,57 @@ namespace Find4
 
         private bool check()
         {
-            List<int> list = new List<int> {0,1,2,3};
+            List<int> listOption = new List<int> {0,1,2,3};
+            List<int> listAns = new List<int> { 0, 1, 2, 3 };
             int black = 0, white = 0;
+            Color col1, col2;
             for (int i = 0; i < 4; i++)
             {
-                if (option.que[i].Fill == panel[round].que[i].Fill)
+                var brush1 = option.que[i].Fill as SolidColorBrush;
+                if (brush1 != null)
+                    col1 = brush1.Color;
+               var brush2 = panel[round].que[i].Fill as SolidColorBrush;
+                if (brush2 != null)
+                    col2 = brush2.Color;
+
+                if (col1 == col2)
                 {
                     black++;
-                    list.RemoveAt(i);
-                }
-                else
-                {
-                    for (int k = 0; k < list.Capacity; k++)
-                    {
-                        if (panel[round].que[i].Fill == option.que[list[k]].Fill)
-                        {
-                            white++;
-                            list.RemoveAt(k);
-                        }
-                    }
+                    listOption.Remove(i);
+                    listAns.Remove(i);
                 }
             }
-                int j;
-                for (j=0; j < black; j++)
-                {
-                    panel[round].ans[j].Fill= color.black;
-                }
-                for (int i = 0; i < white; i++)
-                {
-                    panel[round].ans[j].Fill= color.white;
-                    j++;
-                }
+                
+            for (int i = 0; i < listOption.Count; i++)
+            { 
+                var brush1 = option.que[listOption[i]].Fill as SolidColorBrush;
+                if (brush1 != null)
+                    col1 = brush1.Color;
 
-                return black==4;
+                for (int k = 0; k < listAns.Count; k++) {
+                    var brush2 = panel[round].que[ listAns[k] ].Fill as SolidColorBrush;
+                    if (brush2 != null)
+                        col2 = brush2.Color;
+                    if (col1 == col2)
+                        {
+                            white++;
+                            listAns.Remove( listAns[k] );
+                            break;
+                        }   
+                }        
+            }
+            int j;
+            for (j = 0; j < black; j++)
+            {
+                panel[round].ans[j].Fill = color.black;
+            }
+            for (int i = 0; i < white; i++)
+            {
+                panel[round].ans[j].Fill = color.white;
+                j++;
+            }
+
+            return black==4;
         }
 
 
@@ -134,22 +154,16 @@ namespace Find4
         public class Block
         {
             public Ellipse[] que { get; set; }
-            public Block(/*Ellipse q1, Ellipse q2, Ellipse q3, Ellipse q4*/) 
+            public Block() { }  //////////////////////////////////////////////////
+            public Block(Ellipse q1, Ellipse q2, Ellipse q3, Ellipse q4) 
             {
                 que = new Ellipse[4];
-                /*
                 que[0] = q1;
                 que[1] = q2;
                 que[2] = q3;
                 que[3] = q4;
-                */
-                que[0] = new Ellipse();
-                que[1] = new Ellipse();
-                que[2] = new Ellipse();
-                que[3] = new Ellipse();
-
                 colors col = new colors();
-                Random rand = new Random();     // tha mpourousame na ftiaksoume tin tyxaiotita
+                Random rand = new Random();     
                 for (int i = 0; i < 4; i++)
                 {
                     int option = rand.Next(1, 7);
